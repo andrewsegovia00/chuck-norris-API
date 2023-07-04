2fa6f7e62abc8dcb610f7b49ec77658e12fe2277
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-07-03 21:57:41 -0700

    Deleted unecessary files and updated the routes and donwloaded node-fetch

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', function(req, res, next) {
+  fetch('https://api.chucknorris.io/jokes/random')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      const joke = data.value;
+      res.render('jokes/showJoke', { joke: joke });
+    })
+    .catch(function(error) {
+      console.log(error);
+      res.render('jokes/showJoke', { joke: null });
+    });
+});
+
 
 module.exports = router;
